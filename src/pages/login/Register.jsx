3c424c8eadf7f182e14bbc1d0ee36d933ee50c10
Hasib24 <hasibislam1488@gmail.com>
2023-06-08 01:19:26 +0600

    add hook form npm in regester page

--- src/pages/login/Register.jsx
+++ src/pages/login/Register.jsx
@@ -2,28 +2,41 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import GoogleLoginBtn from '../../components/buttons/GoogleLoginBtn';
+import { useForm } from "react-hook-form";
 
 const Register = () => {
 
     let [show, setShow] = useState(false);
 
-    
-    const handleOnSubmit = (e) =>{
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
 
-    }
+    console.log(errors);
+    
 
 
     return (
         <div className='p-8 my-10 max-w-md mx-auto justify-center border rounded-md shadow-lg'>
 
-            <form className='p-8' onSubmit={handleOnSubmit}>
+            <form className='p-8' onSubmit={handleSubmit(onSubmit)}>
                 
-                <input className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto' type="name" name="name" id="name" placeholder='Enter name' required /> <br />
-                <input className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto' type="email" name="email" id="email" placeholder='Enter email' required /> <br />
-                <input className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto' type={show ? 'password' : 'text'} name="password" id="password" placeholder='Password' required /> <br />
+                <input {...register("name", { required: true })} className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto' type="name" name="name" id="name" placeholder='Enter name'/> <br />
+                {errors.name?.type === 'required' && <p className='text-red-600 -mt-3 pl-1'>Your name is required</p>}
+                
+                <input {...register("email", { required: true })} className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto' type="email" name="email" id="email" placeholder='Enter email' /> <br />
+                {errors.email?.type === 'required' && <p className='text-red-600 -mt-3 pl-1'>Your email is required</p>}
+                
+                <input {...register("password", { required: true })} className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto' type={show ? 'password' : 'text'} name="password" id="password" placeholder='Password'/> <br />
+                
+                
+                <input className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto' type={show ? 'password' : 'text'} name="password" id="password" placeholder='Confirm password'/> <br />
                 <p onClick={()=>{setShow(!show)}}>{show ? <span>show password</span> : <span>hide password</span> }</p>
-                <input className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto' type="url" name="url" id="url" placeholder='Photo url' />
-                <input className='bg-blue-600 border border-blue-600 duration-700 text-white active:bg-blue-500 hover:bg-white hover:text-slate-800 cursor-pointer outline-none rounded-md my-3 w-full md:mx-auto py-2 font-semibold disabled:bg-slate-200' type="submit" name="submit" id="submit" value="Register" />
+                
+                
+                <input {...register("photourl", { required: true })} className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto' type="url" name="photourl" id="photourl" placeholder='Photo url' />
+                {errors.photourl?.type === 'required' && <p className='text-red-600 -mt-3 pl-1'>Photo url is required</p>}
+                
+                <input className='bg-blue-600 border border-blue-600 duration-700 text-white active:bg-blue-500 hover:bg-white hover:text-slate-800 cursor-pointer outline-none rounded-md my-3 w-full md:mx-auto py-2 font-semibold disabled:bg-slate-200' type="submit" name="submit" value="Register" />
 
 
                 <p>Already have an accounte ? <Link to='/login' className='text-blue-600 underline'>Login</Link></p>

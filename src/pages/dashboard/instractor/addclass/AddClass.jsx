04015ca12a94f3880eb4d5aa9d5334a76eecc968
Hasib24 <hasibislam1488@gmail.com>
2023-06-11 18:22:28 +0600

    update add class data of instractor dashboard

--- src/pages/dashboard/instractor/addclass/AddClass.jsx
+++ src/pages/dashboard/instractor/addclass/AddClass.jsx
@@ -38,6 +38,8 @@ const AddClass = () => {
                 instractorName : user.displayName,
                 email : user.email,
                 totalSeat : data.totalSeat,
+                enrolls : 0,
+                feedback : '',
                 price : data.price
             }
             axios.post(`/addaclass?email=${user.email}`, classData)
@@ -106,12 +108,12 @@ const AddClass = () => {
                 <div className="grid my-4 md:grid-cols-2 md:gap-10">
 
                   <div className="relative z-0 w-full mb-6 group">
-                    <input type="text" name="instractorName" id="instractorName" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value='instractor' disabled />
+                    <input type="text" name="instractorName" id="instractorName" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={user.displayName} disabled />
                     <label htmlFor="instractorName" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6" >Instractor Name</label>
                   </div>
                 
                   <div className="relative z-0 w-full mb-6 group">
-                    <input type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value="email@gmail.com" disabled  />
+                    <input type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={user.email} disabled  />
                     <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6" >Instractor EmaiL</label>
                   </div>
 

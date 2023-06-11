92d198a326b4cb0984bb453bdcd3b412e8705481
Hasib24 <hasibislam1488@gmail.com>
2023-06-11 13:44:52 +0600

    img upload to imagebb success

--- src/pages/dashboard/instractor/addclass/AddClass.jsx
+++ src/pages/dashboard/instractor/addclass/AddClass.jsx
@@ -7,11 +7,29 @@ const AddClass = () => {
     const {user} = useContext(AuthContex)
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
+    const imgUploadAPIKey = import.meta.env.VITE_IMAGE_API_KEY;
+    const imgHostingUrl = `https://api.imgbb.com/1/upload?expiration=600&key=${imgUploadAPIKey}`
+    console.log(imgHostingUrl);
+
     const onSubmit = data =>{
+
+        const formData = new FormData()
+        formData.append('image', data.image[0])
+
+        fetch(imgHostingUrl, {
+          method : 'POST',
+          body : formData
+        })
+        .then(response =>response.json())
+        .then(imgResponse =>{
+          console.log(imgResponse.data)
+        })
+
+
         const classData ={
             approveStatus : 'panding',
             className : data.className,
-            photoUrl : data.photoUrl,
+            photoUrl : data.image,
             instractorName : user.displayName,
             email : user.email,
             totalSeat : data.totalSeat,
@@ -41,7 +59,8 @@ const AddClass = () => {
                   </div>
                   {/* TODO: photo upload  */}
                   <div className="relative z-0 w-full mb-6 group">
-                    <input type="file" name="photoUrl" id="photoUrl" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+                    <input {...register("image", { required: true })} type="file" name="image" id="image" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+                    {errors.image?.type === 'required' && <p className='text-red-600 pl-1'>You must select an image</p>}
 
                     <label htmlFor="photoUrl" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6" >Photo URL</label>
                   </div>

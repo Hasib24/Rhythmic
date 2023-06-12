76454bd9ffbd43bdca4977a7c7b64c1ffef71ef9
Hasib24 <hasibislam1488@gmail.com>
2023-06-12 12:28:20 +0600

    fixed feedback button disabled problem

--- src/pages/dashboard/admin/ManageClassesRow.jsx
+++ src/pages/dashboard/admin/ManageClassesRow.jsx
@@ -6,15 +6,19 @@ const ManageClassesRow = ({aClass, index}) => {
     const [feedbackDisable, setFeedbackDisable] = useState(false)
     const [denyDisable, setDenyDisable] = useState(false)
 
+    // console.log(aClass._id);
     const feedbackRef = useRef()
 
-    const handleFeedbackSend =()=>{
+    const handleFeedbackSend =(e, aClass)=>{
+      e.preventDefault()
+      
       const feedbackData = {
         id : aClass._id,
         feedback : feedbackRef.current.value
       }
       
-      axios.post('/feedback', feedbackData)
+      
+      axios.patch('/feedback', feedbackData)
       .then(response =>{
         if(response.data.modifiedCount){
           setFeedbackDisable(true)
@@ -26,10 +30,10 @@ const ManageClassesRow = ({aClass, index}) => {
 
 
     return (
-        <div className="card card-side border my-5 bg-base-100 shadow-xl">
+        <div className="card grid md:grid-cols-2 border my-5 p-4 bg-base-100 shadow-xl">
             
             
-            <img className='rounded-l-lg w-[50%]' src='https://images.pexels.com/photos/164716/pexels-photo-164716.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt="" />
+            <img className='rounded-lg' src='https://images.pexels.com/photos/164716/pexels-photo-164716.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt="" />
             
 
             <div className="card-body">
@@ -43,20 +47,24 @@ const ManageClassesRow = ({aClass, index}) => {
               <div>
                 <button className='btn btn-accent btn-outline mx-2'>Approve</button>
                 {/* Open the modal using ID.showModal() method */}
-                <button className="btn btn-info btn-outline disabled:btn-ghost mx-2" disabled={feedbackDisable}  onClick={()=>window.my_modal_5.showModal()}>send feedback</button>
+                <button className="btn btn-info btn-outline disabled:btn-ghost mx-2" disabled={feedbackDisable}  onClick={()=>window[aClass._id].showModal()}>send feedback</button>
                 
                 <button className='btn btn-error btn-outline mx-2'>Deny</button>
 
-                <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                <dialog id={aClass._id} className="modal modal-bottom sm:modal-middle">
+
                   <form method="dialog" className="modal-box">
                     <h3 className="font-bold text-lg">Feedback</h3>
                     <textarea className="p-2 my-4 outline-none border rounded-md w-full" ref={feedbackRef} name="" id="" ></textarea>
                     <div className="modal-action">
                       {/* if there is a button in form, it will close the modal */}
-                      <button onClick={()=>handleFeedbackSend()} className='btn btn-info btn-outline'>Send</button>
                       <button className="btn btn-outline">Cancle</button>
                     </div>
+                    
+                  <button onClick={(e)=>handleFeedbackSend(e, aClass)} className='btn btn-info btn-outline'>Send</button>
                   </form>
+
+
                 </dialog>
               </div>
             </div>

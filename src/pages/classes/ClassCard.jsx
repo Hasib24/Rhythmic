6c0946ffa3d82c05b25c66a48cdfdbed5a1956bd
Hasib24 <hasibislam1488@gmail.com>
2023-06-13 01:06:00 +0600

    show approved classes to all user

--- src/pages/classes/ClassCard.jsx
+++ src/pages/classes/ClassCard.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const ClassCard = () => {
+const ClassCard = ({aClass}) => {
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-          <figure><img src="https://img.freepik.com/free-photo/latin-girl-with-headphones-listening-her-online-music-lessons-artistic-kid-playing-acoustic-guitar-learning-chords_662251-1427.jpg?w=740&t=st=1686230795~exp=1686231395~hmac=8670cce302d5b337ae70e218ba91675a205b4069035bf75b184bbb6410adc6eb" alt="Shoes" /></figure>
+          <figure><img src={aClass.photoUrl} alt="Shoes" /></figure>
           <div className="card-body">
-            <h2 className="card-title">Class Name</h2>
-            <h2>Instructor</h2>
-            <h2>Available seats</h2>
+            <h2 className="card-title">{aClass.className}</h2>
+            <h2>{aClass.instractorName}</h2>
+            <h2>{aClass.email}</h2>
             <div className="card-actions justify-between">
-                <h2 className='text-2xl font-bold'>Price</h2>
-                <button className="btn btn-primary">Select Now</button>
+                <h2 className='text-2xl font-bold'>{aClass.price}$</h2>
+                <button className="btn btn-info btn-outline">Select Now</button>
             </div>
           </div>
         </div>
--- src/pages/classes/Classes.jsx
+++ src/pages/classes/Classes.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import SectionHeader from '../../components/sectionHeader/SectionHeader';
 import ClassCard from './ClassCard';
+import axios from 'axios';
+import { useQuery } from 'react-query';
 
 const Classes = () => {
+
+    // tenStack query to load all classes 
+    const { isLoading, isError, refetch, data : classesArray = [], error } = useQuery({
+        queryKey : ['allapprovedclasses'],
+        queryFn : async () =>{
+            const response = await axios.get(`/allapprovedclasses`)
+            return response.data
+        }
+    })
+
+    console.log(classesArray);
+
     return (
         <section className='container mx-auto'>
             <SectionHeader>All Classes</SectionHeader>
             <div className='grid md:grid-cols-2 lg:grid-cols-3 py-10'>
-                <ClassCard></ClassCard>
-                <ClassCard></ClassCard>
-                <ClassCard></ClassCard>
+                {
+                    classesArray.map((aClass, index)=><ClassCard key={index} aClass={aClass}></ClassCard>)
+                }
+               
             </div>
         </section>
     );

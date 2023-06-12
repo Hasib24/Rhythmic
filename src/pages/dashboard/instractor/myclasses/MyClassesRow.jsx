0e3a3c754e76638bbfda148b4842249f6ddeea94
Hasib24 <hasibislam1488@gmail.com>
2023-06-12 22:57:52 +0600

    add see notification modal and update design

--- src/pages/dashboard/instractor/myclasses/MyClassesRow.jsx
+++ src/pages/dashboard/instractor/myclasses/MyClassesRow.jsx
@@ -29,13 +29,29 @@ const MyClassesRow = ({aClass, index}) => {
             </td>
             {/* feedback  */}
             <td className="px-6 py-4">
-               <span><AiFillAlert className='inline mr-2'></AiFillAlert>no feedback </span> 
+                {
+                    aClass.feedback ? <div className='relative'>
+                          <span class="animate-ping absolute inline-flex h-1 w-1 rounded-full bg-green-400 opacity-75"></span>
+                          <span onClick={()=>window[aClass._id].showModal()} className='cursor-pointer'><AiFillAlert className='inline mr-2 text-green-600'></AiFillAlert>See feedback </span>
+                    </div>  : <span><AiFillAlert className='inline mr-2'></AiFillAlert>No feedback </span>  
+                }
+
+                {/* Feedback Modal */}
+                
+                <dialog id={aClass._id} className="modal">
+                  <form method="dialog" className="modal-box">
+                    <button htmlFor="my-modal-3" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    <h3 className="font-bold text-lg">Feedback</h3>
+                    <p className="py-4">{aClass.feedback}</p>
+                  </form>
+                </dialog>
+               
                 
             </td>
             {/* status  */}
             <td className="px-6 py-4">
                 {
-                    aClass.approveStatus==='denyed' ? <span> <MdOutlineDoNotDisturb className='mr-2 inline text-red-600'></MdOutlineDoNotDisturb>{aClass.approveStatus} </span> : <span>{aClass.approveStatus==='panding' ? <LuLoader className='mr-2 inline'></LuLoader> : <MdDone className='mr-2 inline text-green-600'></MdDone> }{aClass.approveStatus}</span>
+                    aClass.approveStatus==='denyed' ? <span> <MdOutlineDoNotDisturb className='mr-2 inline text-red-600'></MdOutlineDoNotDisturb>{aClass.approveStatus} </span> : <span>{aClass.approveStatus==='panding' ? <LuLoader className='mr-2 inline animate-spin'></LuLoader> : <MdDone className='mr-2 inline text-green-600'></MdDone> }{aClass.approveStatus}</span>
                 }
                 
                 

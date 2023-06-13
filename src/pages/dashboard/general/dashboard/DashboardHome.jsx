2872aa1fafc9111b55308558edcf5e4a1e145478
Hasib24 <hasibislam1488@gmail.com>
2023-06-13 12:24:22 +0600

    dashboard home design complete

--- src/pages/dashboard/general/dashboard/DashboardHome.jsx
+++ src/pages/dashboard/general/dashboard/DashboardHome.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import SectionHeader from '../../../../components/sectionHeader/SectionHeader';
+import { useContext } from 'react';
+import { AuthContex } from '../../../../providers/AuthProvider';
 
 const DashboardHome = () => {
+    const {user} = useContext(AuthContex)
     return (
-        <div>
-            DashboardHome
-        </div>
-    );
+        <section>
+            <SectionHeader>Welcome to Dashboard</SectionHeader>
+            <div className='flex justify-center my-10'>
+                <div className='flex flex-col items-center w-[50vh] p-5 rounded-lg bg-base-200 border-2 shadow-lg border-base-300  '>
+                    <img className='rounded-full w-1/3' src={user.photoURL} alt="" />
+                    <h1 className='text-xl'>{user.displayName}</h1>
+                    <h1 className='text-xl'>{user.email}</h1>
+                </div>
+            </div>
+        </section>
+    )
+        
 };
 
 export default DashboardHome;

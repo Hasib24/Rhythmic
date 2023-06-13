596b53c5c8ba834c8db848c105547f21b6d01859
Hasib24 <hasibislam1488@gmail.com>
2023-06-13 11:58:52 +0600

    add help page design

--- src/pages/dashboard/general/help/Help.jsx
+++ src/pages/dashboard/general/help/Help.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import SectionHeader from '../../../../components/sectionHeader/SectionHeader';
+import { BiSupport } from 'react-icons/bi';
+import { MdAddCall } from 'react-icons/md';
+import { TfiEmail } from 'react-icons/tfi';
 
 const Help = () => {
     return (
-        <div>
-            help
-        </div>
+        <section>
+            <SectionHeader>Help Desk</SectionHeader>
+            <div>
+                <div className='flex justify-center text-7xl'>
+                    <BiSupport ></BiSupport>24/7
+                </div>
+                <div className='my-10'>
+                    <h1 className='text-center text-3xl'><MdAddCall className='inline mr-2'></MdAddCall>Hot Line : 8809-324XXXXXXX</h1>
+                    <h1 className='text-center text-3xl'><TfiEmail className='inline mr-2'></TfiEmail>Hot Email : suppor@email.com</h1>
+                </div>
+
+            </div>
+        </section>
     );
 };
 

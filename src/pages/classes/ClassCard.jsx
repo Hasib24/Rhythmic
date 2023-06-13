1517b09ba83c8659a0a5a253434a2e02a678516d
Hasib24 <hasibislam1488@gmail.com>
2023-06-13 22:10:22 +0600

    notify before select a class if user is not loged ind

--- src/pages/classes/ClassCard.jsx
+++ src/pages/classes/ClassCard.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContex } from '../../providers/AuthProvider';
+import swal from 'sweetalert';
+import { useNavigate } from 'react-router-dom';
 
 const ClassCard = ({aClass}) => {
+  const navigate = useNavigate()
+
+  const {user} = useContext(AuthContex)
+
+  const handleSelect = (aClass) =>{
+   
+    if(!user){
+      
+      swal({
+        title: "You must login !",
+        text: "Without login you can not select any class",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((ok) => {
+        ok && navigate('/login')
+      })
+    }else{
+      console.log(`Select button clicked`);
+    }
+
+
+
+  }
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
           {
             aClass.totalSeat == 0 ? <div className='bg-red-900 rounded-lg'>
-              <figure><img src={aClass.photoUrl} alt="Shoes" /></figure>
+              <figure><img src={aClass.photoUrl} alt="" /></figure>
             <div className="card-body">
             <h2 className="card-title">{aClass.className}</h2>
             <h2>{aClass.instractorName}</h2>
@@ -25,7 +53,7 @@ const ClassCard = ({aClass}) => {
             <h2>Available Seats : {aClass.totalSeat}</h2>
             <div className="card-actions justify-between">
                 <h2 className='text-2xl font-bold'>{aClass.price}$</h2>
-                <button className="btn btn-info btn-outline">Select Now</button>
+                <button onClick={()=>handleSelect(aClass)} className="btn btn-info btn-outline" >Select Now</button>
             </div>
             </div>
             </div>

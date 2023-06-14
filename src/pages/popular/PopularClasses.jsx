38587531b7298ad2c8c17b267c6b28dec37f0569
Hasib24 <hasibislam1488@gmail.com>
2023-06-14 18:49:44 +0600

    nothing important, just design update

--- src/pages/popular/PopularClasses.jsx
+++ src/pages/popular/PopularClasses.jsx
@@ -10,7 +10,7 @@ const PopularClasses = () => {
                
                 <Slide>
 
-                    <div className='border flex flex-col items-center justify-center p-10'>
+                    <div className='shadow-blue-600 shadow-md flex flex-col items-center justify-center p-10'>
                         <img src="https://assets.yousician.com/app/uploads/2021/02/09140513/yousician-1.jpg" alt="" />
                         <h2 className='font-bold text-2xl my-4'>Learn on a real instrument</h2>
                         <p>All you need is an instrument and the app. No extra accessories like cables or mics needed.</p>
@@ -19,19 +19,19 @@ const PopularClasses = () => {
                
                 <Slide>
 
-                    <div className='border flex flex-col items-center justify-center p-10'>
-                        <img src="https://assets.yousician.com/app/uploads/2021/02/09140513/yousician-1.jpg" alt="" />
-                        <h2 className='font-bold text-2xl my-4'>Learn on a real instrument</h2>
-                        <p>All you need is an instrument and the app. No extra accessories like cables or mics needed.</p>
+                    <div className='shadow-blue-600 shadow-md flex flex-col items-center justify-center p-10'>
+                        <img src="https://assets.yousician.com/app/uploads/2021/02/09140522/yousician-2.jpg" alt="" />
+                        <h2 className='font-bold text-2xl my-4'>For Android, iOS and PC</h2>
+                        <p>Just download the app on your phone, laptop or tablet and you’re good to go.</p>
                     </div>
                 </Slide>
                
                 <Slide>
 
-                    <div className='border flex flex-col items-center justify-center p-10'>
-                        <img src="https://assets.yousician.com/app/uploads/2021/02/09140513/yousician-1.jpg" alt="" />
-                        <h2 className='font-bold text-2xl my-4'>Learn on a real instrument</h2>
-                        <p>All you need is an instrument and the app. No extra accessories like cables or mics needed.</p>
+                    <div className='shadow-blue-600 shadow-md flex flex-col items-center justify-center p-10'>
+                        <img src="https://assets.yousician.com/app/uploads/2021/02/09140529/yousician-3.jpg" alt="" />
+                        <h2 className='font-bold text-2xl my-4'>Go acoustic or electric</h2>
+                        <p>Learn with an acoustic or electric guitar, bass, ukulele or piano.</p>
                     </div>
                 </Slide>
 
--- src/pages/slider/Slider.jsx
+++ src/pages/slider/Slider.jsx
@@ -6,14 +6,14 @@ const Slider = () => {
     return (
         <section>
 
-            <div className='container mx-auto border'>
+            <div className='container mx-auto'>
 
-                <Carousel autoPlay interval={3000} infiniteLoop>
+                <Carousel autoPlay interval={2000} infiniteLoop>
                     <div>
-                        <img className='h-[100%] object-cover w-1/2' src="https://img.freepik.com/free-photo/acoustic-guitar-chair-close-up-brown-guitar-black-wall_1150-21884.jpg?w=740&t=st=1686157880~exp=1686158480~hmac=1a623ac4620ea901c752598e9e5c84da34646a2ee942cbd464dfc372221b8728" />
+                        <img className='h-[100%] object-cover w-1/2' src="https://img.freepik.com/free-photo/string-instruments-close-up-musical-concert-generative-ai_169016-28900.jpg?w=826&t=st=1686746471~exp=1686747071~hmac=e332a86682d63b3d8d6055398c8bf7d4b59a63ac754a2af31087e3e95d992e28" />
                     </div>
                     <div>
-                        <img className='h-[100%] object-cover w-1/2' src="https://img.freepik.com/free-photo/acoustic-guitar-chair-close-up-brown-guitar-black-wall_1150-21884.jpg?w=740&t=st=1686157880~exp=1686158480~hmac=1a623ac4620ea901c752598e9e5c84da34646a2ee942cbd464dfc372221b8728" />
+                        <img className='h-[100%] object-cover w-1/2' src="https://img.freepik.com/free-photo/digital-mixer-recording-studio-with-computer-recording-music-background-drum-set-musical-instruments-concept-creativity-show-business_169016-15568.jpg?w=740&t=st=1686746548~exp=1686747148~hmac=727ea26ca036029bee094ae68703fd3a2b1e7e511be0554c836d73b193e62d11" />
                     </div>
                     <div>
                         <img className='h-[100%] object-cover w-1/2' src="https://img.freepik.com/free-photo/acoustic-guitar-chair-close-up-brown-guitar-black-wall_1150-21884.jpg?w=740&t=st=1686157880~exp=1686158480~hmac=1a623ac4620ea901c752598e9e5c84da34646a2ee942cbd464dfc372221b8728" />

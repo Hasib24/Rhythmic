27800e83da44bb758bbd19e48302dfd662974151
Hasib24 <hasibislam1488@gmail.com>
2023-06-27 13:49:01 +0600

    use test card and show card error

--- src/pages/dashboard/student/payment/CheckoutForm.jsx
+++ src/pages/dashboard/student/payment/CheckoutForm.jsx
@@ -1,9 +1,11 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React from 'react';
+import { useState } from 'react';
 
 const CheckoutForm = () => {
     const stripe = useStripe()
     const elements = useElements();
+    const [cardError, setCardError] = useState('')
 
     const handleSubmit = async (event) => {
         // Block native form submission.
@@ -21,12 +23,26 @@ const CheckoutForm = () => {
           return;
         }
 
+        // console.log(card);
+
+      const {error, paymentMethod} = await stripe.createPaymentMethod({type : 'card', card})
+      if(error){
+        console.log(error)
+        setCardError(error.message)
+      }else{
+        console.log(paymentMethod);
+        setCardError('')
+      }
+
         
 
     }
 
     return (
-    <form onSubmit={handleSubmit}>
+      <>
+      
+      
+    <form onSubmit={handleSubmit} className='border'>
       <CardElement
         options={{
           style: {
@@ -43,10 +59,13 @@ const CheckoutForm = () => {
           },
         }}
       />
-      <button type="submit" disabled={!stripe}>
+      <button type="submit" className='btn btn-outline btn-sm my-4' disabled={!stripe}>
         Pay
       </button>
     </form>
+    <p className='text-red-600 pl-4'>{cardError}</p>
+
+    </>
     );
 };
 

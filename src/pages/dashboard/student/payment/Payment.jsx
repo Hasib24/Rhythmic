f7144e3b0f82fe3f7114debec751e82e2450ec96
Hasib24 <hasibislam1488@gmail.com>
2023-06-27 12:59:07 +0600

    add Stripe checkout form and add PK key after acc creation

--- src/pages/dashboard/student/payment/Payment.jsx
+++ src/pages/dashboard/student/payment/Payment.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import SectionHeader from '../../../../components/sectionHeader/SectionHeader';
+import CheckoutForm from './CheckoutForm';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
 
 const Payment = () => {
     return (
-        <section>
+        <section className='container mx-auto'>
             <SectionHeader>Payment</SectionHeader>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm></CheckoutForm>
+            </Elements>
         </section>
     );
 };

f518b9a670a002b27accd4b6a5fa8e8ab23a19ec
hasib503 <hasibislam1488@gmail.com>
2024-07-26 02:09:02 +0600

    add spinner for loging loding

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,57 +5,53 @@ import { FcGoogle } from 'react-icons/fc';
 import { AuthContex } from '../../providers/AuthProvider';
 import { useForm } from "react-hook-form";
 import axios from 'axios';
+import useTitle from '../../hooks/useTitle';
 
 
 const Login = () => {
+    useTitle('Login')
     const navigate = useNavigate()
-    const { user, setUser, setLoader, signInUser } = useContext(AuthContex)
+    const { user, setUser, loader, setLoader, signInUser } = useContext(AuthContex)
 
-
-   
-
-    //For Visitor as Admin
-    const handleAdminVisitor =()=>{
-        if(localStorage.getItem("User role") =="Admin"){
+    //For Admin
+    const handleAdminVisitor = () => {
+        if (localStorage.getItem("User role") == "Admin") {
             setValue('email', 'admin_visitor@gmail.com');
             setValue('password', 'admin1@#A')
-        }else if(localStorage.getItem("User role") =="Instractor"){
+        } else if (localStorage.getItem("User role") == "Instractor") {
             setValue('email', 'instractor_visitor@gmail.com');
             setValue('password', 'instractor1@#A')
         }
-    } 
-
-    useEffect(()=>{
+    }
+    useEffect(() => {
         handleAdminVisitor()
     }, [])
 
-
     const { register, handleSubmit, watch, setValue, formState: { errors } } = useForm();
     const onSubmit = data => {
-        // console.log(data);
+        setLoader(true)
+
         signInUser(data.email, data.password)
             .then(res => {
-                
+
                 axios.get(`/user/email-login?userEmail=${res.user?.email}&newUser=false`)
                     .then(response => {
                         localStorage.setItem('jwtAccessToken', response.headers.authorization)
                         setUser(response.data)
                         setLoader(null)
                         navigate('/')
-                        
-                        
                     })
                     .catch(error => console.log(error))
             })
             .catch(error => console.log(error))
     }
 
-
-
     return (
         <div className='relative min-h-[100vh] flex justify-center bg-[url("https://images.unsplash.com/photo-1617634795446-b58c985ec639?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-cover'>
-            <div className='mx-3 min-w-[300px] md:mx-auto my-auto p-5 md:px-14 md:py-10 text-slate-100 shadow-2xl rounded-xl  backdrop-blur-3xl border border-slate-600'>
-                <form className='p-8 max-w-md' onSubmit={handleSubmit(onSubmit)}>
+            <div className='mx-3 min-w-[300px] md:mx-auto my-auto p-5 md:px-14 md:py-10 text-slate-100 shadow-2xl rounded-xl  backdrop-blur-3xl border border-slate-600 flex flex-col justify-center'>
+
+                <span className={`loading absolute right-1/2 loading-dots loading-lg ${!loader ? 'opacity-0' : 'opacity-75'}`}></span>
+                <form className={`p-8 max-w-md ${loader ? 'opacity-0' : 'opacity-100'}`} onSubmit={handleSubmit(onSubmit)}>
                     <input {...register("email", { required: true })} className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto bg-inherit placeholder:text-slate-200 border border-slate-600' type="email" name="email" id="email" placeholder='Enter email' />
                     <input {...register("password", { required: true })} className='outline-none border rounded-md my-3 p-2 w-full md:mx-auto bg-inherit placeholder:text-slate-200 border border-slate-600' type="password" name="password" id="password" placeholder='Enter password' />
 
@@ -65,7 +61,7 @@ const Login = () => {
 
 
                 </form>
-                <div className='px-8'>
+                <div className={`p-8 max-w-md ${loader ? 'opacity-0' : 'opacity-100'}`}>
                     <h1 className=' text-center font-bold text-slate-100'>OR</h1>
                     <GoogleLoginBtn><FcGoogle className='inline text-xl mx-2'></FcGoogle>Login with Google</GoogleLoginBtn>
                 </div>
--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -6,7 +6,7 @@ import { AuthContex } from '../providers/AuthProvider';
 const PrivateRoutes = ({ children }) => {
     const {user, loader} = useContext(AuthContex)
     if(loader){
-        return <div className='h-[50vh] flex justify-center items-center'>Loading...</div>
+        return <div className='h-[100vh] w-full flex justify-center items-center'><span className="loading loading-dots loading-lg"></span></div>
     }
     if(user){
         return children
